65769f010adf32296086b4784259a94bd7dc3bea
AeroKevNL <kevin1226@live.nl>
2014-05-27 15:05:44 +0200

    Exercise1f

---
+++ session5/excercise1f.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE Boek (
+	isbn BIGINT UNIQUE,
+	titel VARCHAR(100),
+	auteur VARCHAR(100),
+	PRIMARY KEY(isbn)
+);
+
+CREATE TABLE Exemplaar (
+	isbn BIGINT,
+	volgnummer BIGINT,
+	gewicht FLOAT,
+	kast INT,
+	FOREIGN KEY(isbn) REFERENCES Boek(isbn),
+	PRIMARY KEY(isbn, volgnummer)
+);
+
+CREATE OR REPLACE FUNCTION verwijderISBN() 
+RETURNS TRIGGER
+AS $$ BEGIN
+	DELETE FROM Exemplaar e WHERE e.isbn = OLD.isbn;
+	RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER VerwijderBoek AFTER DELETE ON Boek
+FOR EACH ROW EXECUTE PROCEDURE verwijderISBN();
